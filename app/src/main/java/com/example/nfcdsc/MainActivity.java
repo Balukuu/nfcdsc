ec0bb792e8ccc24f11cc59f3739525a391d9be5a
mikael <michaelajnew@gmail.com>
2021-02-24 13:44:07 +0300

    Testing the intent
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/MainActivity.java
+++ app/src/main/java/com/example/nfcdsc/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.nfcdsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("HWELLO WORLD");
+        testBtn = findViewById(R.id.test_btn);
+
+        testBtn.setOnClickListener(v -> {
+            startActivity(new Intent(this, TestActivity.class));
+        });
+
     }
 }
