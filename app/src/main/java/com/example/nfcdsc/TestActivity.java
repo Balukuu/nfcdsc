b9102fbc65a64a24484d5ea08be9ab6e8a26563d
mikael <michaelajnew@gmail.com>
2021-02-25 10:31:35 +0300

    Fixed the crashing from the last commit.
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/TestActivity.java
+++ app/src/main/java/com/example/nfcdsc/TestActivity.java
@@ -13,7 +13,6 @@ public class TestActivity extends AppCompatActivity {
     NfcManager mNfcManager;
     NfcAdapter mAdapter;
 
-    ImageView sign;
     TextView nfcDetectedMsg;
 
     @Override
@@ -21,18 +20,15 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        sign = findViewById(R.id.sign);
-        nfcDetectedMsg = findViewById(R.id.test_btn);
+        nfcDetectedMsg = findViewById(R.id.nfc_msg);
 
         mNfcManager = (NfcManager) getSystemService(NFC_SERVICE);
-        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        mAdapter = mNfcManager.getDefaultAdapter();
 
         if (mAdapter!=null && mAdapter.isEnabled()){
             nfcDetectedMsg.setText("NFC IS ENABLED");
-            sign.setImageResource(R.drawable.ic_baseline_check_box_24);
         } else {
             nfcDetectedMsg.setText("NFC IS DISABLED");
-            sign.setImageResource(R.drawable.ic_baseline_cancel_presentation_24);
         }
 
     }
