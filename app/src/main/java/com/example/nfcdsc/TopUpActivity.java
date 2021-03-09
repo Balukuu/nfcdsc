7ae88438b0ab0fda8f3256f43fd33dd3b2e48396
mikael <michaelajnew@gmail.com>
2021-03-09 23:36:56 +0300

    Added the tap icon parent view outline and changed the of the button UI Removed the tool bar of the top up activity activity_top_up.xml Added new color in colors.xml Made the top up activity the launcher activity
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/TopUpActivity.java
+++ app/src/main/java/com/example/nfcdsc/TopUpActivity.java
@@ -2,13 +2,23 @@ package com.example.nfcdsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TopUpActivity extends AppCompatActivity {
+
+    private Button topUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_up);
+
+        topUp = findViewById(R.id.write_tag);
+        topUp.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
     }
 }
