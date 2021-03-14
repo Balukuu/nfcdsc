1872b10325da5ee4dc7c2659804bd8bb6649b723
mikael <michaelajnew@gmail.com>
2021-03-14 13:36:46 +0300

    Added the recycler view adapter and a function to populate the recycler view using the data fro the TopUp activity. Added an extra intent to transfer data from the Top Up activity to the PaymentHistory Activity
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/PaymentHistory.java
+++ app/src/main/java/com/example/nfcdsc/PaymentHistory.java
@@ -1,17 +1,49 @@
 package com.example.nfcdsc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.nfcdsc.adapters.RecyclerViewAdapter;
 
 public class PaymentHistory extends AppCompatActivity {
+
+    String account_balance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_history);
+
+        Intent intent = getIntent();
+        account_balance = intent.getStringExtra("MESSAGE");
+        TextView messageView = findViewById(R.id.acc_balance);
+
+        messageView.setText(account_balance);
+
+        populateRecyclerView();
+
+    }
+
+    // METHOD/FUNCTION TO POPULATE THE PAYMENT HISTORY RECYCLER VIEW LIST
+    private void populateRecyclerView() {
+
+        RecyclerView recyclerView = findViewById(R.id.payment_history_list);
+
+        Intent stringIntent = getIntent();
+
+        String data = stringIntent.getStringExtra("MESSAGE");
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, data);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     public void viewSettings(View view){
--- app/src/main/java/com/example/nfcdsc/TopUpActivity.java
+++ app/src/main/java/com/example/nfcdsc/TopUpActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TopUpActivity extends AppCompatActivity {
 
@@ -18,7 +20,22 @@ public class TopUpActivity extends AppCompatActivity {
 
         topUp = findViewById(R.id.write_tag);
         topUp.setOnClickListener(v -> {
-            startActivity(new Intent(this, PaymentHistory.class));
+                EditText amount = findViewById(R.id.note);
+
+                String msg = amount.getText().toString();
+
+                if (!msg.isEmpty()) {
+                    Intent intent = new Intent(this, PaymentHistory.class);
+                    intent.putExtra("MESSAGE", msg);
+                    startActivity(intent);
+
+                    amount.setText("");
+
+                } else {
+                    Toast.makeText(this, " ENTER ANY AMOUNT ",
+                            Toast.LENGTH_SHORT).show();
+                    amount.requestFocus();
+                }
         });
     }
 }
