2b3456538191ee8b2c20547150cfd1189adad635
mikael <michaelajnew@gmail.com>
2021-03-19 23:49:34 +0300

    Modified the settings UI removed the list view and implemented constraint layout
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/SettingsActivity.java
+++ app/src/main/java/com/example/nfcdsc/SettingsActivity.java
@@ -2,7 +2,16 @@ package com.example.nfcdsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.nfcdsc.adapters.RecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +19,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Intent stringIntent = getIntent();
+
+        String data = stringIntent.getStringExtra("AMOUNT CHARGED");
+
     }
 }
--- app/src/main/java/com/example/nfcdsc/SignUp.java
+++ app/src/main/java/com/example/nfcdsc/SignUp.java
@@ -2,13 +2,48 @@ package com.example.nfcdsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    //Global variables for UI components
+    private Button signup;
+    private EditText fname, lname, phoneNo;
+
+    //Global variables
+    private String firstname, lastname, phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Binding the xml to java
+        signup = findViewById(R.id.sign_up_btn);
+        fname = findViewById(R.id.first_name);
+        lname = findViewById(R.id.second_name);
+        phoneNo = findViewById(R.id.phone_number);
+
+        signup.setOnClickListener(v -> {
+
+            firstname = fname.getText().toString();
+            lastname = lname.getText().toString();
+            phone = phoneNo.getText().toString();
+
+            if (!firstname.isEmpty() && !lastname.isEmpty() && !phone.isEmpty()) {
+                Intent intent = new Intent(this, SettingsActivity.class);
+                intent.putExtra("MESSAGE", firstname);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, " ENTER YOUR DETAILS ",
+                        Toast.LENGTH_SHORT).show();
+                fname.requestFocus();
+            }
+        });
+
     }
 }
