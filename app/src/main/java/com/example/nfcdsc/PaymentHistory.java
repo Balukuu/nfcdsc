df4dd2244f0ba1966baa025ee309e2c22124ba13
mikael <michaelajnew@gmail.com>
2021-03-20 17:37:11 +0300

    Added the Bottom Nav Bar to the PaymentHistory Activity
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/nfcdsc/PaymentHistory.java
+++ app/src/main/java/com/example/nfcdsc/PaymentHistory.java
@@ -6,10 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.nfcdsc.adapters.RecyclerViewAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PaymentHistory extends AppCompatActivity {
 
@@ -45,7 +48,9 @@ public class PaymentHistory extends AppCompatActivity {
         account_balance_txt.setText(account_balance);
         //messageView.setText((int) current_amount);
 
+        //FUNCTIONS.
         populateRecyclerView();
+        handleBottomNavBarActions();
 
     }
 
@@ -63,6 +68,36 @@ public class PaymentHistory extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    // Function handling the bottom nav bar
+    private void handleBottomNavBarActions(){
+
+        //Handling the Bottom navigation view actions
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_nav_bar);
+
+        // CHECKING THE CURRENT CLICKED BOTTOM NAV BAR MENU ITEM
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.tap2pay:
+                    startActivity(new Intent(this, MainActivity.class));
+                    break;
+                case R.id.topup_activity:
+                    startActivity(new Intent(this, TopUpActivity.class));
+                    break;
+                case R.id.payment_history_activity:
+                    break;
+                case R.id.profile:
+                    startActivity(new Intent(this,SettingsActivity.class));
+                    break;
+            }
+            return false;
+        });
     }
 
     public void viewSettings(View view){
